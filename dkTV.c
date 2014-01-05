26c4f6ac993e2e261cdbd57a67ec33358d38f9b5
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-01-05 22:26:56 +0100

    Some changes to dkTV and to gitignore

---
+++ dkTV.c
@@ -0,0 +1,67 @@
+
+/**
+ *
+ *	dkTV.c
+ *	Created on 5.1.2014.
+ *
+ *	@author Daniel Rufus Kaldheim <daniel@kaldheim.org>
+ *	@copyright 2010 - 2014 Crudus Media
+ *	@version 1.0.0
+ *
+ */
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <gdk/gdkx.h>
+#include "gtkplayer.h"
+
+
+static gint quit(GtkWidget *widget, GdkEventAny *event,
+                                 gpointer data)
+{
+	GtkPlayer * p = (GtkPlayer *)data;
+	gtk_player_stop(p);
+	exit(0);
+}
+
+int main(int argc,char *argv[])
+{
+	/*we need a window*/
+	GtkWidget *main_window, *box;
+	/*and a player*/
+	GtkPlayer *player;
+
+	if(argc < 2) {
+		printf("usage: %s file\n",argv[0]);
+		exit(-1);
+	}
+	/*standard gtk stuff*/
+	gtk_init(&argc,&argv);
+	main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	box = gtk_vbox_new(FALSE,2);
+	/*create player and integrate it*/
+	player = gtk_player_new(argv[1]);
+
+	gtk_container_add(GTK_CONTAINER(main_window), box);
+	/* you could also don't use a box
+	 * or use gtk_container_add(GTK_CONTAINER(box), player->widget);
+	 */
+	gtk_box_pack_start(GTK_BOX(box), player->widget,TRUE,TRUE,0);
+
+        gtk_signal_connect(GTK_OBJECT(main_window), "delete_event",
+                           GTK_SIGNAL_FUNC(quit), player);
+
+	/*init player*/
+	gtk_player_init(player);
+	gtk_widget_show(main_window);
+	gtk_widget_show(box);
+	/*show player (the parent tree has to show)*/
+	gtk_player_show(player);
+	/*start player*/
+	gtk_player_start(player);
+	gtk_main();
+}
+
+
+/* End of file dkTV.c */
+/* Location: ./dkTV.c */
