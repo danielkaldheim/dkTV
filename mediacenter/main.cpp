fff25b3ce4e2e3075707cae7d4e4aa41d9e0ab0a
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-01-15 21:12:55 +0100

    Initial Commit

---
+++ mediacenter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mediacenter
+//
+//  Created by Daniel Rufus Kaldheim on 15.01.14.
+//  Copyright (c) 2014 Crudus MEdia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
